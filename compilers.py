0a53f4a6058a857b43be5d833d0d9ee4777c9c2d
Matthew Hagan <matthewhagan15@gmail.com>
2022-03-14 22:15:36 -0400

    first and second order trotter working

--- compilers.py
+++ compilers.py
@@ -1,4 +1,5 @@
 import numpy as np
+from scipy import linalg
 
 # A basic trotter simulator organizer.
 # Inputs
@@ -17,20 +18,51 @@ class TrotterSim:
         self.time = time
         self.iterations = iterations
         self.spectral_norms = []
-        self.hilbert_dim = hamiltonian_list[0].shape()[0]
+        self.hilbert_dim = hamiltonian_list[0].shape[0]
         self.compute_spectral_norms()
 
     # Helper function to compute spectral norm list. Used in constructor, isolated as function for
     # potential use besides internally. If used externally returns copy so it cannot be modified
     # externally
     def compute_spectral_norms(self):
-        if self.spectral_norms.len() == 0:
-            ret = [np.linalg.norm(h, ord=2) for h in hamiltonian_list]
+        if len(self.spectral_norms) == 0:
+            ret = [np.linalg.norm(h, ord=2) for h in self.hamiltonian_list]
             self.spectral_norms = ret 
         return self.spectral_norms.copy()
 
     # Computes and stores the total approximate unitary evolution operater e^{i H t} based on the
     # order and parameters as given.
-    def generate_evolution_op(self):
+    def first_order_op(self):
         evol_op = np.identity(self.hilbert_dim)
+        for h in self.hamiltonian_list:
+            exp_h = linalg.expm(1.0j * self.time / (1.0 * self.iterations) * h)
+            evol_op = np.matmul(evol_op, exp_h)
+        return np.linalg.matrix_power(evol_op, int(self.iterations))
+    
+    def second_order_op(self):
+        evol_op = np.identity(self.hilbert_dim)
+        self.time = self.time/2.0
+        evol_op = np.matmul(evol_op, self.first_order_op())
+        self.time = self.time * -1.0
+        evol_op = np.matmul(evol_op, self.first_order_op().conj().T)
+        self.time = -2.0 * self.time
         return evol_op
+
+# Create a simple evolution operator, compare the difference with known result. Beware floating pt
+# errors
+# H = sigma_X
+def test_first_order_op():
+    sigma_x = np.array([[0,1],[1,0]])
+    print(sigma_x)
+    sim = TrotterSim([sigma_x], time=np.pi/2, iterations=1)
+    sim2 = TrotterSim([sigma_x], time=np.pi/2, iterations=2)
+    print(sim.first_order_op())
+    print(sim2.first_order_op())
+
+def test_second_order_op():
+    sigma_x = np.array([[0, 1], [1, 0]])
+    sim = TrotterSim([sigma_x], time=np.pi/2, iterations=1)
+    print(sim.second_order_op())
+
+test_first_order_op()
+test_second_order_op()
